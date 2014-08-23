39ecb36b16cc9c581fbb2e04dd1b4f581a422237
bananatron <bananatrongo@gmail.com>
2014-08-23 01:05:28 +0000

    vote validations

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,8 +1,9 @@
 class Vote < ActiveRecord::Base
-  #validates :user_id, :review_id, presence: true
+  validates :user_id, :review_id, presence: true
   
   belongs_to :user
   belongs_to :review
 
+  #Ensures there can only be one upvote per user for a given review
   validates :user_id, :uniqueness => { :scope => :review_id }
 end
