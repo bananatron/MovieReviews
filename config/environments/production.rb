ec7e9f09145b742b24ee4f9c6426653ad72e7a2c
bananatron <bananatrongo@gmail.com>
2014-09-05 22:54:02 +0000

    static_assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,7 +87,7 @@ Rails.application.configure do
   
   #CSS Load issues
   #config.cache_classes = true
-  #config.serve_static_assets = true
+  config.serve_static_assets = true
   #config.assets.compile = true
   #config.assets.digest = true
   
