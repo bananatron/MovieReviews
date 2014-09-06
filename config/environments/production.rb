9d0f41be42420c419c7dec282593c93fea1e6193
bananatron <bananatrongo@gmail.com>
2014-09-06 00:08:15 +0000

    modal fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,10 +23,11 @@ Rails.application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
+  # Had to disable because css wasn't being loaded on heroku
   #config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = :uglifier
+  #config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
