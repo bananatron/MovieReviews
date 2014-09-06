ce412e860a1d2c0f6e56eb7baa999505c788be34
bananatron <bananatrongo@gmail.com>
2014-09-06 01:21:08 +0000

    pass mailer

--- Gemfile
+++ Gemfile
@@ -1,5 +1,9 @@
 source 'https://rubygems.org'
 
+group :development, :test do
+gem 'foreman'
+end
+
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'devise'
 gem 'rails', '4.1.4'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,6 +84,28 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
+  #Devise
+  ::SITE_DOMAIN = "tldwmovies.com"
+  
+  config.action_mailer.default_url_options = { :host => 'tldrmovies.herokuapp.com' }
+  #if heroku errors do this
+  #Rails.application.routes.default_url_options[:host] = 'yoursite.herokuapp.com'
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default :charset => "utf-8"
+
+    config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: ENV["GMAIL_DOMAIN"],
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+    }
+  
   #CSS Load issues
   #config.cache_classes = true
   #config.serve_static_assets = true
