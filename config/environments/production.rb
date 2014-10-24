8fad1352e75f16494063cc4aadb7778e96191210
bananatron <bananatrongo@gmail.com>
2014-10-24 15:44:24 +0000

    google analytics

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  GA.tracker = "UA-xxxxxx-x"
   # Set to true for production
   config.cache_classes = true
 
